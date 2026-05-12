008c886a5c013cd2108112f53b59f85c0b66bb5f
StanLy369 <stanislavtombrachevici@gmail.com>
2026-05-13 00:36:49 +0300

    Сделал UML диаграмму
    Добавил engine_rules это скажем так, менеджер правил, их логику пропишу в файле rules.py

---
+++ GUI/rules_engine.py
@@ -0,0 +1,27 @@
+import re
+# from typing import List, Dict, Optional
+from rules import BaseRule, RegexRule, ExtensionRule
+
+
+class EngineRules:
+    def __init__(self):
+        self.rules = []  # тут будет храниться правила сортировки
+
+    def add_rule(self, rule):
+        #   метод для добавления правила сортировки
+        self.rules.append(rule)
+
+    def get_destination(self, filename):
+        # через имя файла получу нахождение того, для чего применю правило
+        """Возвращает папку назначения для файла или None, если правило не найдено"""
+        for rule in self.rules:
+            if rule.match(filename):
+                return rule.destination
+            return None  # файл не подходит ни под одно правило
+
+    def load_from_config(self, config):
+        """Загружает правила из конфигурационного файла (пока заглушка)"""
+        pass
+
+
+
